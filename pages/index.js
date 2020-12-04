65a7574a1e2fd5704bfe31c67a9c0c9e1dafd946
KanexKane <kanexkane@gmail.com>
2020-12-04 22:47:18 +0700

    success

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,70 @@
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import DataMenus from '../public/menus.json'
+
 export default function Home() {
+  const numberDaysInWeek = 7
+  const initialMenus = Array(numberDaysInWeek).fill('')
+  const [menus, setMenus] = useState(initialMenus)
+
+  const daysInWeek = [
+    'จันทร์',
+    'อังคาร',
+    'พุธ',
+    'พฤหัส',
+    'ศุกร์',
+    'เสาร์',
+    'อาทิตย์',
+  ]
+
+  const randomMenus = () => {
+    const countMenuAll = DataMenus.length
+
+    const nowMenu = []
+
+    for (let i = 0; i < numberDaysInWeek; i++) {
+      let nowRandomNumber
+
+      do {
+        nowRandomNumber = Math.floor(Math.random() * Math.floor(countMenuAll))
+      } while (nowMenu.includes(nowRandomNumber))
+
+      nowMenu[i] = nowRandomNumber
+    }
+
+    setMenus(nowMenu)
+  }
+
+  useEffect(() => {
+    randomMenus()
+  }, [])
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>สุ่มรายการอาหารทั้งสัปดาห์</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div style={{ width: '80vh', margin: '2em auto' }}>
+        <h1 style={{ textAlign: 'center' }}>สุ่มรายการอาหารทั้งสัปดาห์!</h1>
+        {menus.map((item, index) => {
+          if (typeof item == 'number') {
+            return (
+              <div key={index} className='menu-container'>
+                <div className='menu-day'>วัน{daysInWeek[index]}:</div>
+                <div className='menu-name'>{DataMenus[item].name}</div>
+              </div>
+            )
+          }
+        })}
+      </div>
+      <div style={{ textAlign: 'center' }}>
+        <button className='btn-random' onClick={randomMenus}>
+          สุ่มรายการอาหารหน่อย
+        </button>
+      </div>
     </div>
   )
 }
